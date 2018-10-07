b2f62848d38819d1b3adfd88ba3f2410c6a8218a
monofon <monofon@tramberend.de>
2018-10-07 15:07:47 +0200

    Optimze structure of Main

--- src/tramberend7215/a01/Main.java
+++ src/tramberend7215/a01/Main.java
@@ -3,22 +3,39 @@ package tramberend7215.a01;
 import cgtools.Vec3;
 import static cgtools.Vec3.*;
 import java.io.IOException;
+import java.util.function.*;
 import tramberend7215.Image;
 
 public class Main {
-    static int width = 160;
-    static int height = 90;
 
     public static void main(String[] args) {
+        final int width = 160;
+        final int height = 90;
+
         Image image = new Image(width, height);
 
-        for (int x = 0; x != width; x++) {
-            for (int y = 0; y != height; y++) {
-                image.setPixel(x, y, pixelColor(x, y));
+        class ConstantColor {
+            Vec3 color;
+
+            ConstantColor(Vec3 color) {
+                this.color = color;
+            }
+
+            Vec3 pixelColor(double x, double y) {
+                return color;
             }
         }
 
-        String filename = "doc/a01.png";
+        ConstantColor allGray = new ConstantColor(Vec3.gray);
+        for (int x = 0; x != width; x++) {
+            for (int y = 0; y != height; y++) {
+                image.setPixel(x, y, allGray.pixelColor(x, y));
+            }
+        }
+        write(image, "doc/a01.png");
+    }
+
+    static void write(Image image, String filename) {
         try {
             image.write(filename);
             System.out.println("Wrote image: " + filename);
@@ -27,7 +44,4 @@ public class Main {
         }
     }
 
-    static Vec3 pixelColor(double x, double y) {
-        return vec3(0.5, 0.5, 0.5);
-    }
 }

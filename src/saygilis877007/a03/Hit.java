c93a72185e20bfdf2d5233f247d420b00d896f06
C3M-O- <home@homes-MacBook-Pro.local>
2018-10-29 10:56:30 +0100

    compareTo wurde hinzugefügt

--- src/saygilis877007/a03/Hit.java
+++ src/saygilis877007/a03/Hit.java
@@ -4,19 +4,20 @@ import cgtools.Vec3;
  * Created by home on 29.10.18.
  */
 public class Hit implements Comparable <Hit>{
-    public double strahlParameter;
-    public Vec3 trefferPunkt;  // Position des Trefferpunkts
-    public Vec3 normalerPunkt;  // Normalenvektor der Oberflaeche
+    public double t; //Strahlparmater
+    public Vec3 x;  //Position des Trefferpunkts
+    public Vec3 n;  // Normalenvektor
+    public Vec3 c; //Farbe der Oberfläche
 
 
-    public Hit(double strahlParameter, Vec3 trefferPunkt, Vec3 normalerPunkt){
-        this.strahlParameter = strahlParameter;
-        this.trefferPunkt = trefferPunkt;
-        this.normalerPunkt = normalerPunkt;
+    public Hit(double t, Vec3 x, Vec3 n){
+        this.t = t;
+        this.x = x;
+        this.n = n;
     }
 
     @Override
     public int compareTo(Hit o) {
-        return new Double(strahlParameter).compareTo(o.strahlParameter);
+        return new Double(t).compareTo(o.t);
     }
 }

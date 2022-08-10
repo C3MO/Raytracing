14a9a9f5f01299a075a7b3e0bf8319aaf36b410e
Cem Saygili <cem@pr0kommunal.com>
2022-08-10 14:34:08 +0200

    [fix] add documentation for methods
    - remove redundant call 'format()'
    - change value fields to 'final'
    - remove idea conf files
    - add values for gitignore

--- src/saygilis877007/a01/ColoredSquare.java
+++ src/saygilis877007/a01/ColoredSquare.java
@@ -26,18 +26,31 @@ public class ColoredSquare {
         Image image = new Image(width, height);
 
 
+        /**
+         * The class colorChoice is a class that takes in a color and returns a color based on the position of the pixel
+         */
         class colorChoice {
-            Vec3 color;
+            final Vec3 color;
 
+            // A constructor that takes in a color and assigns it to the variable color.
             colorChoice(Vec3 color) {
                 this.color = color;
             }
 
 
+            /**
+             * If the pixel is in the middle of the image, it returns a color. If it is not, it returns a different color
+             *
+             * @param x The x coordinate of the pixel
+             * @param y The y coordinate of the pixel.
+             * @return The color of the pixel.
+             */
             Vec3 pixelColor(int x, int y) {
 
                 double zposition = height / d;
                 z = x / squarePart + y / squarePart;
+                // This is a conditional statement that checks if the pixel is in the middle of the image. If it is, it
+                // returns a color.
                 if ((x > width / c - zposition) && (x < width / c + zposition) && (y > height / c - zposition) && (y < height / c + zposition)) {
                     return new Vec3(1, 0, 0.9);
                 } else if (z % c == b) {
@@ -52,7 +65,10 @@ public class ColoredSquare {
         }
 
 
+        // Creating a new instance of the class colorChoice and assigning it to the variable color.
         colorChoice color = new colorChoice(Vec3.white);
+        // This is a nested for loop that goes through every pixel in the image and sets the color of the pixel to the
+        // color that is returned by the pixelColor method.
         for (int x = 0; x != width; x++) {
             for (int y = 0; y != height; y++) {
                 image.setPixel(x, y, color.pixelColor(x, y));
@@ -61,12 +77,18 @@ public class ColoredSquare {
         write(image, "doc/a01-checkered-background.png");
     }
 
+    /**
+     * It writes an image to a file
+     *
+     * @param image the image to write
+     * @param filename The name of the file to write to.
+     */
     static void write(Image image, String filename) {
         try {
             image.write(filename);
             System.out.println("Wrote image: " + filename);
         } catch (IOException err) {
-            System.out.println(String.format("Something went wrong writing: ", filename, err));
+            System.out.printf("Something went wrong writing: %n", filename, err);
         }
     }
 
--- src/saygilis877007/a01/Main.java
+++ src/saygilis877007/a01/Main.java
@@ -13,9 +13,14 @@ public class Main {
     static int a = 10;
     static int b = 2;
 
+    /**
+     * It creates a new image, sets the color of each pixel, and then writes the image to a file
+     */
     public static void main(String[] args) {
         Image image = new Image(width, height);
 
+        // This is a nested for loop. The outer loop is iterating over the width of the image, and the inner loop is
+        // iterating over the height of the image. The pixelColor method is being called for each pixel in the image.
         for (int i = 0; i != width; i++) {
             for (int j = 0; j != height; j++) {
                 image.setPixel(i, j, pixelColor(i, j));
@@ -27,16 +32,26 @@ public class Main {
             image.write(filename);
             System.out.println("Wrote image: " + filename);
         } catch (IOException err) {
-            System.out.println(String.format("Something went wrong writing ", filename, err));
+            System.out.printf("Something went wrong writing %n", filename, err);
         }
     }
 
-    static Vec3 pixelColor(int x, int y) {      //new vector with the given coordinates and choose color
+    /**
+     * If the value of z is divisible by b, then return a vector with the x, y, and z values of 0, 0, and 0. Otherwise,
+     * return a vector with the x, y, and z values of 1, 1, and x
+     *
+     * @param x The x value of the pixel.
+     * @param y The y value of the pixel.
+     * @return The pixelColor method is returning a vector with the x, y, and z values of 0, 0, and 0.
+     */
+    static Vec3 pixelColor(int x, int y) {
         z = x / a + y / a;
         if (z % b == 0) {
-            return new Vec3(0, 0, 0); // Schachbrettmuster (Black)
+            // Returning a new vector with the x, y, and z values of 0, 0, and 0.
+            return new Vec3(0, 0, 0);
         } else {
-            return new Vec3(1, 1, (x)); //Schachbrettmuster (White)
+            // Creating a new vector with the x, y, and z values of 1, 1, and x.
+            return new Vec3(1, 1, (x));
         }
 
     }
